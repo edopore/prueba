d4cff898b5753994ea020ee1d83ad004590de36a
edopore <ediij1994@hotmail.com>
2018-03-18 20:54:23 -0500

    bueno comentario

--- app/src/main/java/com/edopore/prueba/MainActivity.java
+++ app/src/main/java/com/edopore/prueba/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    //El comentario de arriba es el mas nuevo
     //Agrego otro comenttario
     // esto es un comentario
 }
