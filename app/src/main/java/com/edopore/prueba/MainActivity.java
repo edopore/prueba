03c1a4f6807408739bf9ffeb91bc3bbc38c87c76
edopore <ediij1994@hotmail.com>
2018-03-18 20:40:07 -0500

    bueno comentario

--- app/src/main/java/com/edopore/prueba/MainActivity.java
+++ app/src/main/java/com/edopore/prueba/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // esto es un comentario
     //Agrego otro comenttario
+    // esto es un comentario
 }
